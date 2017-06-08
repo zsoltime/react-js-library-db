49efd2667bc9faedbfe326b8e464b64ba54a8cee
Zsolt Meszaros <zsolt@sowad.com>
2017-06-08 19:38:10 +0100

    Upgrade react, update UI, BEMify CSS, add modal

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-function convertToApiUrl(url) {
-  return url.replace(/https:\/\/github.com\/(.*)\/(.*)/i, (m, name, repo) => (
+const convertToApiUrl = (url) => {
+  return url.replace(/https:\/\/github.com\/(.*)\/(.*)/i, (_, name, repo) => (
     `https://api.github.com/repos/${name}/${repo}`
   ));
 }
 
-const libraries = [
+const initialLibraries = [
   {
     name: 'React',
     repository: 'facebook/react',
     description: 'A declarative, efficient, and flexible JavaScript library for building user interfaces.',
     homepage: 'https://facebook.github.io/react/',
+    readmeURL: 'https://api.github.com/repos/facebook/react/readme',
     avatar: 'https://avatars.githubusercontent.com/u/69631?v=3',
     stats: {
       createdAt: '2013-05-24T16:15:54Z',
@@ -25,6 +26,7 @@ const libraries = [
     repository: 'reactjs/redux',
     description: 'Predictable state container for JavaScript apps',
     homepage: 'http://redux.js.org',
+    readmeURL: 'https://api.github.com/repos/reactjs/redux/readme',
     avatar: 'https://avatars.githubusercontent.com/u/6412038?v=3',
     stats: {
       createdAt: '2015-05-29T23:53:15Z',
@@ -40,6 +42,7 @@ const libraries = [
     repository: 'driftyco/ionic',
     description: 'Build amazing native and progressive web apps with Angular and open web technologies. One app running on everything 🎉',
     homepage: 'http://ionicframework.com/',
+    readmeURL: 'https://api.github.com/repos/driftyco/ionic/readme',
     avatar: 'https://avatars.githubusercontent.com/u/3171503?v=3',
     stats: {
       createdAt: '2014-09-18T16:12:01Z',
@@ -55,6 +58,7 @@ const libraries = [
     repository: 'moment/moment',
     description: 'Parse, validate, manipulate, and display dates in javascript.',
     homepage: 'http://momentjs.com',
+    readmeURL: 'https://api.github.com/repos/moment/moment/readme',
     avatar: 'https://avatars.githubusercontent.com/u/4129662?v=3',
     stats: {
       createdAt: '2011-03-01T02:46:06Z',
@@ -70,6 +74,7 @@ const libraries = [
     repository: 'meteor/meteor',
     description: 'Meteor, the JavaScript App Platform',
     homepage: 'https://www.meteor.com',
+    readmeURL: 'https://api.github.com/repos/meteor/meteor/readme',
     avatar: 'https://avatars.githubusercontent.com/u/789528?v=3',
     stats: {
       createdAt: '2012-01-19T01:58:17Z',
@@ -85,6 +90,7 @@ const libraries = [
     repository: 'vuejs/vue',
     description: 'A progressive, incrementally-adoptable JavaScript framework for building UI on the web.',
     homepage: 'http://vuejs.org',
+    readmeURL: 'https://api.github.com/repos/vuejs/vue/readme',
     avatar: 'https://avatars.githubusercontent.com/u/6128107?v=3',
     stats: {
       createdAt: '2013-07-29T03:24:51Z',
@@ -98,16 +104,39 @@ const libraries = [
   },
 ];
 
+const Modal = (props) => (
+  <div className="modal-overlay">
+    <div className="modal">
+      <button
+        className="modal__btn modal__btn--close"
+        onClick={props.onClose}
+        aria-label="Close"
+      >&times;</button>
+      {props.children}
+    </div>
+  </div>
+);
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchTerm: '',
-      libraries,
+      libraries: props.initialLibraries,
+      isModalOpen: false,
     };
     this.filterResults = this.filterResults.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.onEscape = this.onEscape.bind(this);
+  }
+  componentDidMount() {
+    addEventListener('keydown', this.onEscape);
+  }
+  componentWillUnmount() {
+    removeEventListener('keydown', this.onEscape);
   }
   filterResults(item) {
     if (this.state.searchTerm) {
@@ -123,13 +152,14 @@ class App extends React.Component {
   }
   handleSubmit(url) {
     axios.get(convertToApiUrl(url))
-    .then(res => {console.log(res); return res.data;})
+    .then(res => res.data)
     .then(res => ({
       id: res.id,
       name: res.name,
       repository: res.full_name,
       description: res.description,
       homepage: res.homepage,
+      readmeURL: `${res.url}/readme`,
       avatar: res.owner.avatar_url,
       stats: {
         createdAt: res.created_at,
@@ -143,39 +173,71 @@ class App extends React.Component {
     }))
     .then(repo => {
       this.setState(prevState => ({
-        libraries: [...prevState.libraries, repo],
+        libraries: [repo, ...prevState.libraries],
+        searchTerm: '',
+        isModalOpen: false,
       }))
     });
   }
   openModal() {
-    //
+    this.setState({
+      isModalOpen: true,
+    })
+  }
+  closeModal() {
+    this.setState({
+      isModalOpen: false,
+    })
+  }
+  onEscape(e) {
+    if (e.keyCode === 27) {
+      this.closeModal();
+    }
   }
   render() {
     const listItems = this.state.libraries
-    .filter(this.filterResults)
-    .map(item => {
-      return (
-        <li key={item.repository} className="item">
-          <img className="item__image" src={item.avatar} />
+      .filter(this.filterResults)
+      .map(item => (
+        <li key={item.repository} className="library">
+          <img className="library__image" src={item.avatar} />
           <section>
-            <h2 className="item__name">{item.name}</h2>
-            <div className="item__description">
+            <h2 className="library__name">{item.name}</h2>
+            <div className="library__description">
               {item.description}
             </div>
           </section>
         </li>
-      );
-    });
+      ));
     return (
-      <div className="container">
-        <header>
-          <SearchForm handleChange={this.handleChange} />
-          <button onClick={this.openModal}>Add a New Library</button>
-        </header>
-        <SubmitForm handleSubmit={this.handleSubmit}/>
-        <ul className="items">
+      <div>
+        <div className="large-hero">
+          <div className="container container--small">
+            <div className="logo">JS</div>
+            <h1 className="large-hero__title">Find the Best JavaScript Libraries and Frameworks</h1>
+            <section className="search-bar">
+              <SearchForm handleChange={this.handleChange} />
+            </section>
+          </div>
+        </div>
+        <ul className="libraries">
           {listItems}
         </ul>
+        {this.state.isModalOpen && (
+          <Modal onClose={this.closeModal}>
+            <h2 className="modal__title">Add a New Library</h2>
+            <div className="modal__content">
+              <p>Can't see your library listed?</p>
+              <p>Just type or paste your GitHub repo's URL below!</p>
+            </div>
+            <SubmitForm handleSubmit={this.handleSubmit}/>
+          </Modal>
+        )}
+        <button
+          className="btn btn--add"
+          onClick={this.openModal}
+          type=""
+          aria-label="Add Library"
+        >+</button>
       </div>
     );
   }
@@ -200,10 +262,14 @@ class SearchForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
   }
-  render () {
+  render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form
+        className="form form--search"
+        onSubmit={this.handleSubmit}
+      >
         <input
+          className="form__field form__field--search"
           type="search"
           placeholder="Search JS libraries and frameworks"
           defaultValue={this.state.value}
@@ -227,6 +293,9 @@ class SubmitForm extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  componentDidMount() {
+    this.inputRef.focus();
+  }
   handleChange(event) {
     this.setState({
       repo: event.target.value,
@@ -238,14 +307,22 @@ class SubmitForm extends React.Component {
   }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form
+        className="form form--submit"
+        onSubmit={this.handleSubmit}
+      >
         <input
+          className="form__field form__field--submit"
           type="url"
           placeholder="Enter a valid GitHub link"
           defaultValue={this.state.repo}
           onChange={this.handleChange}
+          ref={node => { this.inputRef = node; }}
         />
-        <button type="submit">Add It</button>
+        <button
+          className="btn btn--submit"
+          type="submit"
+        >Add Library</button>
       </form>
     );
   }
@@ -255,4 +332,7 @@ SubmitForm.propTypes = {
   handleSubmit: React.PropTypes.func.isRequired,
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <App initialLibraries={initialLibraries}/>,
+  document.getElementById('app')
+);
--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # WIP React JS Library Database
 
-Javascript library and framework listing app.
+Javascript library and framework listing app. You can add more libraries by
+simply entering their Github URL. The app fetches the necessary details to
+display.
+
+It uses [axios](https://github.com/mzabriskie/axios) instead of fetch.
+
+## Next steps
+
+- [ ] Add [react-transition-group](https://github.com/reactjs/react-transition-group)
+- [ ] Error handling
+- [ ] Save libraries to database
+- [ ] Display stars, forks, readme, etc
+- [ ] Add categories
